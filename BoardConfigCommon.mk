aa513fd424915b7a0813bbc6cfc1dd20fc3246c0
Sultanxda <sultanxda@gmail.com>
2013-08-13 16:22:27 -0700

    Remove ION

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,6 @@ TARGET_CPU_VARIANT := scorpion
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
-TARGET_USES_ION := true
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
