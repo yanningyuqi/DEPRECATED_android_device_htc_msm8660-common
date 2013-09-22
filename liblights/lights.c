46df741f930a442b939895ff71d0fd666dc4bb81
Sultanxda <sultanxda@gmail.com>
2013-09-21 19:28:17 -0700

    Revert "[1/2] msm8660: liblights: Enable userspace control over buttons backlight"
    This reverts commit dbda5c0ece0dd02fc170733c84fc7b8cc1752fa4.

--- liblights/lights.c
+++ liblights/lights.c
@@ -118,12 +118,6 @@ static int is_lit (struct light_state_t const* state) {
 	return state->color & 0x00ffffff;
 }
 
-static int rgb_to_brightness(struct light_state_t const* state)
-{
-    int color = state->color & 0x00ffffff;
-    return ((77*((color>>16)&0x00ff))
-            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
-}
 
 static void set_speaker_light_locked (struct light_device_t *dev, struct light_state_t *state) {
 	unsigned int colorRGB = state->color & 0xFFFFFF;
@@ -223,9 +217,9 @@ static void handle_speaker_battery_locked (struct light_device_t *dev) {
 static int set_light_buttons (struct light_device_t* dev,
 		struct light_state_t const* state) {
 	int err = 0;
-	int brightness = rgb_to_brightness(state);
+	int on = is_lit (state);
 	pthread_mutex_lock (&g_lock);
-	err = write_int (BUTTON_FILE,brightness);
+	err = write_int (BUTTON_FILE, on?255:0);
 	pthread_mutex_unlock (&g_lock);
 
 	return 0;
@@ -238,7 +232,7 @@ static int set_light_keyboard(struct light_device_t* dev,
 	pthread_mutex_lock(&g_lock);
 	err = write_int(KEYBOARD_FILE, on?255:0);
 	pthread_mutex_unlock(&g_lock);
-
+	
 	return 0;
 }
 
@@ -262,8 +256,15 @@ static int set_light_func(struct light_device_t* dev,
 	g_func = on;
 	err = write_int(FUNC_LED_FILE, on?255:0);
 	pthread_mutex_unlock(&g_lock);
-
+	
 	return 0;
+}
+
+static int rgb_to_brightness(struct light_state_t const* state)
+{
+    int color = state->color & 0x00ffffff;
+    return ((77*((color>>16)&0x00ff))
+            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
 }
 
 static int set_light_backlight(struct light_device_t* dev,
