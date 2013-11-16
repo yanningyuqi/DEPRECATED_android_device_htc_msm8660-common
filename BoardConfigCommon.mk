6494a0ad467628c4054a6662363eb4d7304d69fd
Sultanxda <sultanxda@gmail.com>
2013-11-16 12:53:22 -0800

    Add working sepolicy (credit: dferg)

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,3 +116,32 @@ TARGET_FORCE_CPU_UPLOAD := true
 
 # Hardware tunables
 BOARD_HARDWARE_CLASS := device/htc/msm8660-common/cmhw/
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/msm8660-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file_contexts \
+    files \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    untrusted_app.te \
+    vold.te \
+    wpa.te \
+    wpa_socket.te
