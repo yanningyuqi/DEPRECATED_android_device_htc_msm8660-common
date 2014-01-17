e7c0e0d1043c838e335cdb299793dfe8c2e3e7b9
Sultan (android1234567) <sultanxda@gmail.com>
2014-01-16 21:21:09 -0800

    BoardConfigCommon: Use optimized Krait routines

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -27,7 +27,7 @@ TARGET_BOARD_PLATFORM := msm8660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 # Architecture
-TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_ARMV7A := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
